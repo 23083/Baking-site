f208f80bc4ac9baca73747f9e2f943b31b3cd806
23083 <23083@burnside.school.nz>
2026-04-24 14:30:00 +1200

    connected to data and tests

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+DATABASE = 'database.db'
 
+#initialise app
 app = Flask(__name__)
 
-@app.route("/")
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.route('/')
 def home():
-    #homepage
-    return "Hello, Again!"
+    #home page - just the ID, Category, RecipeName and Image
+    sql = """
+                SELECT Recipe.RecipeID,Category.Name,Recipe.RecipeName,Recipe.ImageURL
+                FROM Recipe
+                JOIN Category ON Category.CategoryID=Recipe.CategoryID;"""
+    results = query_db(sql)
+    return str(results)
+
+
+@app.route("/recipe/<int:id>")
+def recipe(id):
+    #just one recipe based on the id
+    sql = """SELECT * FROM Recipe 
+    JOIN Category ON Category.CategoryID=Recipe.CategoryID
+    WHERE Recipe.RecipeID = ?;"""
+    result = query_db(sql,(id,),True)
+    return str(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
